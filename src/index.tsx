f9b6a73ba885795deed21e23abcdbbb133dc7437
zhenia-chugaev <zhenia.chugaev@bk.ru>
2023-05-31 17:38:54 +0300

    Initialize Firebase app

--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
+import { initializeApp } from 'firebase/app';
 import App from './App';
+import './index.css';
 
-const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
-);
+const firebaseConfig = JSON.parse(process.env.REACT_APP_FIREBASE_CONFIG as string);
+const firebaseApp = initializeApp(firebaseConfig);
+console.log(firebaseApp);
+
+const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
+
 root.render(
   <React.StrictMode>
     <App />
