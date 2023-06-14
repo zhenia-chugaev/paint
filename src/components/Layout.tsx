487b889e87a4a25671faf6a9114f79dae1b0aeb0
zhenia-chugaev <zhenia.chugaev@bk.ru>
2023-06-14 15:06:07 +0300

    Add layout components

---
+++ src/components/Layout.tsx
@@ -0,0 +1,61 @@
+import Box from '@mui/material/Box';
+import Typography from '@mui/material/Typography';
+import Link from '@mui/material/Link';
+import type { BoxProps } from '@mui/material/Box';
+
+const App = (props: BoxProps) => (
+  <Box
+    sx={{
+      display: 'flex',
+      flexDirection: 'column',
+      height: '100vh',
+    }}
+    {...props}
+  />
+);
+
+const Header = (props: BoxProps) => (
+  <Box
+    sx={{
+      display: 'flex',
+      alignItems: 'center',
+      minHeight: 60,
+      py: 1,
+    }}
+    component="header"
+    {...props}
+  >
+    {props.children}
+  </Box>
+);
+
+const Main = (props: BoxProps) => (
+  <Box sx={{ flexGrow: 1 }} component="main" {...props} />
+);
+
+const Footer = (props: BoxProps) => (
+  <Box
+    sx={{
+      py: 1,
+      minHeight: 50,
+      textAlign: 'center',
+    }}
+    component="footer"
+    {...props}
+  >
+    <Typography
+      sx={{
+        color: 'text.secondary',
+        fontSize: '0.9rem',
+        lineHeight: '50px',
+      }}
+    >
+      View the{' '}
+      <Link color="text.primary" href="https://github.com/zhenia-chugaev/paint">
+        source code
+      </Link>
+    </Typography>
+  </Box>
+);
+
+export { App, Header, Main, Footer };
---
+++ src/components/index.tsx
@@ -0,0 +1,3 @@
+import { App, Header, Main, Footer } from './Layout';
+
+export { App, Header, Main, Footer };
