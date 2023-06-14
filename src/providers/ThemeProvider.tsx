678da12b3274450b55257fde061e935d2d735f7c
zhenia-chugaev <zhenia.chugaev@bk.ru>
2023-06-14 15:02:13 +0300

    Connect theme provider with the store

--- src/providers/ThemeProvider.tsx
+++ src/providers/ThemeProvider.tsx
@@ -1,12 +1,17 @@
 import { ThemeProvider as ColorThemeProvider } from '@mui/material/styles';
+import { useTypedSelector } from '../hooks';
 import getTheme from '../themes';
 
 interface Props {
   children?: React.ReactNode;
 }
 
-const ThemeProvider = ({ children }: Props) => (
-  <ColorThemeProvider theme={getTheme('dark')}>{children}</ColorThemeProvider>
-);
+const ThemeProvider = ({ children }: Props) => {
+  const mode = useTypedSelector((state) => state.theme.mode);
+
+  return (
+    <ColorThemeProvider theme={getTheme(mode)}>{children}</ColorThemeProvider>
+  );
+};
 
 export default ThemeProvider;
