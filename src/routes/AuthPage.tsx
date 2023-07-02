87223f0010124890880082bcbb0919a45697f515
zhenia-chugaev <zhenia.chugaev@bk.ru>
2023-07-02 18:14:30 +0300

    Fix app navigation

--- src/routes/AuthPage.tsx
+++ src/routes/AuthPage.tsx
@@ -1,13 +1,17 @@
-import { Link, Navigate } from 'react-router-dom';
+import { Link, Navigate, useLocation } from 'react-router-dom';
 import { useTypedSelector } from '../hooks';
 import { Header, Main, Footer, Logo, AuthForm } from '../components';
 import { routes } from './';
 
 const AuthPage = () => {
   const user = useTypedSelector((state) => state.auth.user);
+  const location = useLocation();
+
+  const searchParams = new URLSearchParams(location.state?.from?.search);
+  const previousPage = searchParams.get('page');
 
   return user ? (
-    <Navigate to={routes.feed()} />
+    <Navigate to={routes.feed(Number(previousPage))} />
   ) : (
     <>
       <Header>
