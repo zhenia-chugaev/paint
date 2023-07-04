9dcb485a31b7e4c8383efbe075f2a5487133bff4
zhenia-chugaev <zhenia.chugaev@bk.ru>
2023-07-04 16:16:21 +0300

    Add managing changes of auth state

--- src/routes/Root.tsx
+++ src/routes/Root.tsx
@@ -1,13 +1,31 @@
+import { useEffect } from 'react';
 import { Outlet } from 'react-router-dom';
+import { getAuth, onAuthStateChanged } from 'firebase/auth';
 import Container from '@mui/material/Container';
+import { useTypedDispatch } from '../hooks';
+import { setUser } from '../slices/authSlice';
 import { App } from '../components';
 
-const Root = () => (
-  <Container maxWidth="md">
-    <App>
-      <Outlet />
-    </App>
-  </Container>
-);
+const Root = () => {
+  const dispatch = useTypedDispatch();
+
+  useEffect(() => {
+    return onAuthStateChanged(getAuth(), (user) => {
+      if (user) {
+        dispatch(setUser(user));
+      } else {
+        dispatch(setUser(null));
+      }
+    });
+  }, [dispatch]);
+
+  return (
+    <Container maxWidth="md">
+      <App>
+        <Outlet />
+      </App>
+    </Container>
+  );
+};
 
 export default Root;
