1cca81fc443376ea57a84c16f1b92ded09a6d9b9
zhenia-chugaev <zhenia.chugaev@bk.ru>
2023-07-14 13:44:03 +0300

    Add handling of editor form states

--- src/routes/EditorPage.tsx
+++ src/routes/EditorPage.tsx
@@ -1,5 +1,5 @@
 import { useState, useEffect } from 'react';
-import { Link, Form } from 'react-router-dom';
+import { Link, Form, Navigate } from 'react-router-dom';
 import { useForm } from 'react-hook-form';
 import Box from '@mui/material/Box';
 import TextField from '@mui/material/TextField';
@@ -8,8 +8,9 @@ import Tooltip from '@mui/material/Tooltip';
 import Divider from '@mui/material/Divider';
 import InfoOutlined from '@mui/icons-material/InfoOutlined';
 import { useTypedSelector, useTypedDispatch } from '../hooks';
-import { saveDrawing } from '../slices/dataSlice';
+import { saveDrawing, resetRequestStatus } from '../slices/dataSlice';
 import { Header, Main, Logo, Editor, Footer, Message } from '../components';
+import { routes } from './';
 import type { SubmitHandler } from 'react-hook-form';
 
 interface Inputs {
@@ -20,6 +21,7 @@ const EditorPage = () => {
   const [isMobileDevice, setIsMobileDevice] = useState<boolean>(false);
   const user = useTypedSelector((state) => state.auth.user);
   const editor = useTypedSelector((state) => state.editor);
+  const requestStatus = useTypedSelector((state) => state.data.requestStatus);
   const { register, handleSubmit } = useForm<Inputs>();
   const dispatch = useTypedDispatch();
 
@@ -28,6 +30,11 @@ const EditorPage = () => {
       setIsMobileDevice(true);
     }
   }, []);
+
+  useEffect(
+    (): (() => void) => () => dispatch(resetRequestStatus()),
+    [dispatch]
+  );
 
   const onSubmit: SubmitHandler<Inputs> = (inputs) => {
     const drawing = {
@@ -38,45 +45,56 @@ const EditorPage = () => {
     dispatch(saveDrawing(drawing));
   };
 
-  return (
+  return requestStatus === 'succeeded' ? (
+    <Navigate to={routes.feed()} />
+  ) : (
     <>
       <Header>
         <Logo component={Link} to="/" />
       </Header>
       <Main pb={6}>
-        <Box
-          sx={{
-            display: 'flex',
-            flexWrap: { xs: 'wrap', sm: 'nowrap' },
-            alignItems: 'center',
-            gap: { xs: 2, sm: 3 },
-            py: 2,
-          }}
-          component={Form}
-          onSubmit={handleSubmit(onSubmit)}
-        >
-          <TextField
-            {...register('name')}
-            label="Name"
-            id="name"
-            defaultValue="My drawing"
-            variant="filled"
-            size="small"
-            disabled={isMobileDevice}
-            fullWidth
-          />
-          <Tooltip title="You won't be able to edit the drawing anymore." arrow>
-            <Button
-              sx={{ minWidth: { xs: 1, sm: 160 } }}
-              type="submit"
-              variant="contained"
-              color="primary"
+        <Form onSubmit={handleSubmit(onSubmit)}>
+          <Box
+            sx={{
+              display: 'flex',
+              flexWrap: { xs: 'wrap', sm: 'nowrap' },
+              alignItems: 'center',
+              gap: { xs: 2, sm: 3 },
+              py: 1,
+            }}
+          >
+            <TextField
+              {...register('name')}
+              label="Name"
+              id="name"
+              defaultValue="My drawing"
+              variant="filled"
+              size="small"
               disabled={isMobileDevice}
+              fullWidth
+              error={requestStatus === 'failed'}
+            />
+            <Tooltip
+              title="You won't be able to edit the drawing anymore."
+              arrow
             >
-              Save & Quit
-            </Button>
-          </Tooltip>
-        </Box>
+              <Button
+                sx={{ minWidth: { xs: 1, sm: 160 } }}
+                type="submit"
+                variant="contained"
+                color="primary"
+                disabled={isMobileDevice}
+              >
+                {requestStatus === 'loading' ? 'Saving...' : 'Save & Quit'}
+              </Button>
+            </Tooltip>
+          </Box>
+          {requestStatus === 'failed' && (
+            <Message sx={{ mb: 1 }} variant="caption">
+              Something went wrong. Try again.
+            </Message>
+          )}
+        </Form>
 
         <Divider sx={{ mb: 2 }} />
 
